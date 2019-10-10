993be30b08eab6e7646c3eb71d6758d51a54d2bb
Derek <nightwing917@hotmail.com>
2019-10-09 20:01:16 -0600

    itemUpdateSubmit

--- app.js
+++ app.js
@@ -185,6 +185,9 @@ const App = (function(ItemCtrl, UICtrl){
 
     // Edit icon click event
     document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
+
+    // Update item event
+    document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
   }
 
   // Add item submit
@@ -212,7 +215,7 @@ const App = (function(ItemCtrl, UICtrl){
     e.preventDefault();  
   }
 
-  // Update item submit
+  // Click edit item
   const itemEditClick = function(e){
     if(e.target.classList.contains('edit-item')){
       // Get list item id (item-0, item-1)
